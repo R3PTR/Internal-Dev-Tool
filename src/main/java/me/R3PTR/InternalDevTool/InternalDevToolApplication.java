bae71742cd7dd9b516527be1e1fc55fc9cf202cc
R3PTR <levin.backes@gmail.com>
2022-01-20 21:28:02 +0100

    Initial commit

---
+++ src/main/java/me/R3PTR/InternalDevTool/InternalDevToolApplication.java
@@ -0,0 +1,13 @@
+package me.R3PTR.InternalDevTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternalDevToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternalDevToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/R3PTR/InternalDevTool/InternalDevToolApplicationTests.java
@@ -0,0 +1,13 @@
+package me.R3PTR.InternalDevTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternalDevToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
